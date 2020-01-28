9bb7819de458c9fb5e373b6b30b6c016b0718a91
Al-x-R <alexander.roshchin85@gmail.com>
2020-01-28 12:03:42 +0200

    Create model Reservation

--- config/reviews/models.py
+++ config/reviews/models.py
@@ -13,3 +13,6 @@ class Review(core_models.TimeStampedModel):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     room = models.ForeignKey('rooms.Room', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.review} - {self.room}'
+
